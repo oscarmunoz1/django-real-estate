8d01a0dffa9278a5c970d9fa17c4b8158623476f
Oscar Muñoz <osmunogue@gmail.com>
2022-01-09 16:15:18 -0300

    Initial settings.

---
+++ real_estate/settings/development.py
@@ -0,0 +1,20 @@
+from .base import *
+import os
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": env("POSTGRES_ENGINE"),
+#         "NAME": env("POSTGRES_DB"),
+#         "USER": env("POSTGRES_USER"),
+#         "PASSWORD": env("POSTGRES_PASSWORD"),
+#         "HOST": env("PG_HOST"),
+#         "PORT": env("PG_PORT"),
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
